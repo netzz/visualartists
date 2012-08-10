ada118652dfc8a888da1b5a60116f2554128ba17
Eugene Afonin <netzz@visualartists.ru>
2012-08-11 00:36:12 +0400

    Trying opencv with unity

--- unityOpencvPlugin/VS2008Plugin/Plugin.cpp
+++ unityOpencvPlugin/VS2008Plugin/Plugin.cpp
@@ -1,10 +1,11 @@
 
 #include <iostream>
-#include <XnOpenNI.h>
-#include <XnCppWrapper.h>
-#include <XnFPSCalculator.h>
 #include <sstream>
 #include <fstream>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
 using namespace std;
 
 #define SAMPLE_XML_PATH "./SamplesConfig.xml"
@@ -27,76 +28,61 @@ using namespace std;
 // ------------------------------------------------------------------------
 // Plugin itself
 
-using namespace xn;
-
 // Link following functions C-style (required for plugins)
+using namespace cv;
 extern "C"
 {
 
+	
+	float g_pDepthHist[MAX_DEPTH];
+	VideoCapture *cap;
+	Mat img;
 
-float g_pDepthHist[MAX_DEPTH];
-  Context context;
-            ScriptNode g_scriptNode;
-            DepthGenerator depth;
-            ImageGenerator g_image;
-            DepthMetaData g_depthMD;
-            ImageMetaData g_imageMD;
-		XnFPSData xnFPS;
-
-// The functions we will call from Unity.
-//
 	const EXPORT_API char*  StartNI(){
-		XnStatus rc;
 
-		EnumerationErrors errors;
-		rc = context.InitFromXmlFile(SAMPLE_XML_PATH, g_scriptNode, &errors);
-	
-		//return ("Hello1" + rc );
-		rc = context.FindExistingNode(XN_NODE_TYPE_DEPTH, depth);
-		rc= xnFPSInit(&xnFPS, 180);
-		return "Init ok";
+		//namedWindow("window");	
+		cap=new VideoCapture(0);
+		if (cap->isOpened())
+		{
+			return "Init ok";
+		} else {
+			return "Ok";
+		}
 	}
-	
-	const EXPORT_API int UpdateNI(unsigned short * data ){
-		XnStatus rc;
-		rc = context.WaitOneUpdateAll(depth);
-		xnFPSMarkFrame(&xnFPS);
 
-		depth.GetMetaData(g_depthMD);
-		const XnDepthPixel* pDepthMap = g_depthMD.Data();
+		const EXPORT_API void getframe(){
 
-		//std::ostringstream res;
-		  ofstream myfile;
-			myfile.open ("example.txt");
-			myfile << "Writing this to a file.\n";
-			 
-			myfile << "Frame " << g_depthMD.FrameID() << " FPS: " << xnFPSCalc(&xnFPS) << " Middle point is: " << g_depthMD(g_depthMD.XRes() / 2, g_depthMD.YRes() / 2) << " " << g_depthMD.XRes() << " " << g_depthMD.YRes() << endl;
-		myfile.close();
-		//string res1=res.str();
+		}
+
+	const EXPORT_API int UpdateNI(unsigned char * data ){
+
+		*(cap) >> img;
+		//imshow ("window", img);
+		ofstream myfile;
+		/*
+		myfile.open ("example.txt");
 		
-		int n=0;
+		myfile <<  img.size().width << endl;
+		myfile <<  img.size().height << endl;
+
+		myfile.close();
+		*/
+		uchar *c=img.data;
+		memcpy(data, img.data, img.size().width*img.size().height*3);
+	/*		
 		for (int i=0;i<g_depthMD.XRes()*g_depthMD.YRes();i++)
 		{
 			data[i]=pDepthMap[i];
 		}
 
 		return 0;
+		*/
+		return 0;
 	}
 	const EXPORT_API char*  StopNI(){
-	depth.Release();
-	context.Release();
-	return "";
+
+		destroyWindow("window");
+		return "";
 	}
-int EXPORT_API PrintANumber(){
-	return 199;
-}
-
-int EXPORT_API AddTwoIntegers(int a, int b) {
-	return a + b;
-}
-
-float EXPORT_API AddTwoFloats(float a, float b) {
-	return a + b;
-}
 
 } // end of export C block
